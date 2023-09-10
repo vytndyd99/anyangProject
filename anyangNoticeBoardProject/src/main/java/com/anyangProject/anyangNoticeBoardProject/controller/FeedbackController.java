edd5d3ed4717e3c435d0ffa9e0def3956c6e9873
pyosuyong <41277173+vytndyd99@users.noreply.github.com>
2023-09-10 21:28:38 +0900

    23.09.10

---
+++ anyangNoticeBoardProject/src/main/java/com/anyangProject/anyangNoticeBoardProject/controller/FeedbackController.java
@@ -0,0 +1,14 @@
+package com.anyangProject.anyangNoticeBoardProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/develop")
+public class FeedbackController {
+    @GetMapping("/feedback")
+    public String feedbackPage() {
+        return "feedbackPage";
+    }
+}
